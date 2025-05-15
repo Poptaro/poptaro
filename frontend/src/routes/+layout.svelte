fad3da62e9fb0b2be8e79e96d6051f50480bd80e
poptaro <noahahn199@gmail.com>
2025-05-15 16:43:19 -0500

    first initial commit with intellisense issues

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+</script>
+
+<div class="flex justify-between items-center px-8 w-full bg-gray-800 h-12">
+	<div class="text-orange-500">
+		Poptaro
+	</div>
+	<div>
+		:D
+	</div>
+</div>
+
+{@render children()}
---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<div>
+  <h1 class="">
+    ABOUYT PAGE
+  </h1>
+</div>
